176f7638a66048574eaa6939cde3d3db17627d37
Deepak Katariya <deepak.sorthiya@gmail.com>
2026-05-23 22:38:38 +0530

    update AppConfig to disable proxyBeanMethods for improved performance

--- src/main/java/com/example/config/AppConfig.java
+++ src/main/java/com/example/config/AppConfig.java
@@ -7,7 +7,7 @@ import com.example.beans.ExampleBeanTwo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class AppConfig {
 
     @Bean(initMethod = "customInit", destroyMethod = "customDestroy")
